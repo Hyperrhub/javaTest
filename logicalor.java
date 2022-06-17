3c5bdbfe4e9e3bbbedd52b47f55574302b703ee7
Hyperrhub <107431447+Hyperrhub@users.noreply.github.com>
2022-06-17 12:33:28 +0530

    Add files via upload

---
+++ logicalor.java
@@ -0,0 +1,13 @@
+public class logicalor{
+public static void main(String arg[])
+{
+
+int a=13;
+int b=35;
+int c=8;
+
+System.out.println(a<b && ++a>c);
+System.out.println(a);
+System.out.println(!(a>c));
+}
+}
